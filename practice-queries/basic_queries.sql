9aeee41614951b8c448ce79d5d1cf37f50ab5751
KotaMounika <mounikakota2006@gmail.com>
2026-06-01 20:32:07 +0530

    Update basic_queries.sql

--- practice-queries/basic_queries.sql
+++ practice-queries/basic_queries.sql
@@ -1,1 +1,13 @@
+----Select employees table
 SELECT * FROM employees;
+
+---Employees with high salary
+SELECT name
+FROM Employees
+WHERE Salary > 50000;
+
+---Sort employees by salary
+SELECT * 
+FROM Employees
+ORDER BY Salary DESC;
+
