42c445099ebe66bf1c92186cb94e98602b960adc
KotaMounika <mounikakota2006@gmail.com>
2026-06-02 19:00:35 +0530

    Update basic_queries.sql

--- practice-queries/basic_queries.sql
+++ practice-queries/basic_queries.sql
@@ -11,3 +11,12 @@ SELECT *
 FROM Employees
 ORDER BY Salary DESC;
 
+--- get job details of both data analyst and business analyst positions
+-- for data analyst i want jobs only >100k
+-- for business analyst i want jobs >70k also include located in either boston, ma or anywhere
+SELECT *, FROM job_postings
+WHERE job_location IN ('Boston','MA','Anywhere') AND ((job ='Data Analyst' AND salary > 100000) OR (job='Business Analyst' AND salary > 70000));
+
+
+
+
